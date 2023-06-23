13b6347468c5c57f5d0d6047f325f8f65ce5f2be
CodewithNiki <101751820+CodewithNiki@users.noreply.github.com>
2023-06-23 04:13:49 +0100

    update

--- chat/src/lib/firebase.jsx
+++ chat/src/lib/firebase.jsx
@@ -1,10 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { firebaseConfig,  } from "./firebase.config"
-// import { connectAuthEmulator } from "firebase/auth";
+import { connectAuthEmulator } from "firebase/auth";
 
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-// connectAuthEmulator(auth, "http://localhost:9099");
+connectAuthEmulator(auth, "http://localhost:9099");
 
